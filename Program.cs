24d46b1ff5bd5cae7e28f71a23fa973274cf24b8
Allie <allison.tpttn@gmail.com>
2019-10-10 21:36:55 -0500

    working filtered operations

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linqed_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            List<string> LFruits = fruits.Where(fruit => fruit.StartsWith("L")).ToList();
+            //where n.StartsWith("S") 
+
+            Console.WriteLine("----fruits with L-----");
+
+            foreach (string fruit in LFruits)
+            {
+                Console.WriteLine(fruit);
+            }
+
+            Console.WriteLine();
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            List<int> fourSixMultiples = numbers.Where(num => num % 4 == 0 || num % 6 ==0).ToList(); //didn't work without the ToList()
+            // var result = list.Where(i => i%2 == 0);
+
+            foreach (int num in fourSixMultiples)
+            {
+              Console.WriteLine(num);  
+            }
+
+
+
+
+
+
+        }
+    }
+}
